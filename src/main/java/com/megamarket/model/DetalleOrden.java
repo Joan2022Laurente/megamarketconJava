b5c81dfc6261fb8a40c23a330c39dfeac17b3c76
joanjoaquin <joancallanaupalaurente@gmail.com>
2023-01-18 18:02:24 -0500

    oaaa

---
+++ src/main/java/com/megamarket/model/DetalleOrden.java
@@ -0,0 +1,9 @@
+package com.megamarket.model;
+
+public class DetalleOrden {
+	private Integer id;
+	private String nombre;
+	private double cantidad;
+	private double precio;
+	private double total;
+}
---
+++ src/test/java/com/megamarket/MegaMarketttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaMarketttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
